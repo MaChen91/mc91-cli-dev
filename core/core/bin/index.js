8505778c0c027f0689919bb944f62794c511ce21
MaChen91 <bj060421@126.com>
2022-11-21 19:56:34 +0800

    add core

---
+++ core/core/bin/index.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+'use strict';
+const importLocal = require('import-local')
+if (importLocal(__filename)) {
+    const log = require('npmlog')
+    log.info('cli', '正在使用 mc91-cli-dev 本地版本')
+} else { 
+    require('../lib')(process.argv.slice(2))
+}
