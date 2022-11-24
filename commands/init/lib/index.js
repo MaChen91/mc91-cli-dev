f8403b6c19f6c68663ef4166428876221e7b7c58
MaChen91 <bj060421@126.com>
2022-11-24 17:10:52 +0800

    add fix

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -9,6 +9,5 @@ function init(...args) {
   const [name, option, cmd] = args;
   log.info('projectName', name);
   log.info('option', option);
-  //log.info('command', cmd);
   log.info('targetPath', process.env.CLI_TARGET_PATH);
 }
